b92387c5f2cf4f17562a7547028f14bc66476731
DiegoGenuino <genuinodiego11@gmail.com>
2024-09-20 17:07:41 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Mostrar senha digitada no input
-function togglePassword() {
-    var passwordField = document.getElementById("senha");
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-    } else {
-        passwordField.type = "password";
-    }
-}
 
 // Mostrar o campo de Broker quando o modo Cliente (MQTT) for selecionado
 document.getElementsByName('modo').forEach(radio => {
@@ -26,6 +17,17 @@ document.getElementsByName('usar-firebase').forEach(radio => {
             document.getElementById('firebase-auth').style.display = 'block';
         } else {
             document.getElementById('firebase-auth').style.display = 'none';
+        }
+    });
+});
+
+// Mostrar o campo de Broker quando o modo Cliente (MQTT) for selecionado
+document.getElementsByName('ToggleDinamico').forEach(radio => {
+    radio.addEventListener('change', function () {
+        if (this.value === 'cliente') {
+            document.getElementById('broker-div').style.display = 'block';
+        } else {
+            document.getElementById('broker-div').style.display = 'none';
         }
     });
 });
