0cc14c62251f0a0c36f694a71933741a39928771
DiegoGenuino <genuinodiego11@gmail.com>
2024-09-23 23:49:14 -0300

    Revert "Commit"
    This reverts commit 47d998e7578bdc675d7cbdaccf82c9e63d71d08f, reversing
    changes made to 8a3a4cae336213bd0aa95a4827e6f971792b34b2.

--- script.js
+++ script.js
@@ -27,10 +27,7 @@ document.getElementsByName('usar-firebase').forEach(radio => {
 // Mostrar ou esconder os campos de IP estático
 document.getElementsByName('ip-type').forEach(radio => {
     radio.addEventListener('change', function () {
-        if (this.value === 'cliente') {
-            document.getElementById('broker-div').style.display = 'block';
-        } else {
-            document.getElementById('broker-div').style.display = 'none';
-        }
-    });
-});
+        const ipDetalhesDiv = document.getElementById('options');
+        ipDetalhesDiv.style.display = this.value === 'estatico' ? 'block' : 'none';
+    });
+    });
